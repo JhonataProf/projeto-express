9bf4c5b7a8ef95564738cd988801de618fbd98eb
Jhonata Sobral <jhonata.sobral@ms.senac.br>
2024-04-17 21:43:57 -0400

    query podologo

--- src/controllers/lista-podologo.js
+++ src/controllers/lista-podologo.js
@@ -4,8 +4,13 @@ import { PodologoSchema } from "../schema/podologo.js"
 export class ListaPodologo{
     async list(req, res) {
         try {
+            const queryParams = req.query
             const podologoRepository = AppDataSource.getRepository(PodologoSchema)
-            const podologos = await podologoRepository.find()
+            const podologos = await podologoRepository.find({
+                where: {
+                    ...queryParams
+                }
+            })
             return res.json(podologos)
         } catch (error) {
             res.status(500).json({message: error.message})
