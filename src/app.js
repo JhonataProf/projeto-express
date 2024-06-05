bd2ca30553eb1702ade0befb8c751577c5acc54c
Jhonata Sobral <jhonata.sobral@ms.senac.br>
2024-06-05 19:10:04 -0400

    set port application

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import { AppDataSource } from './app-data-source.js'
 import { router } from './router.js'
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(router())
